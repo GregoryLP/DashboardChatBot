2ff27114069ca6a7d5edc208f0fc03882d4e138e
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2024-04-11 11:52:07 +0200

    encore un autre test

--- chatbot.py
+++ chatbot.py
@@ -38,10 +38,13 @@ class CustomModel(tf.keras.Model):
     def __init__(self):
         super(CustomModel, self).__init__()
         self.embedding_layer = CustomEmbeddingLayer(input_dim, output_dim, input_length=max_seq_length)
-        # Add other layers of your model here
-        self.dense_layer = tf.keras.layers.Dense(128, activation='relu')
         self.rnn_layer = CustomRNNLayer(128)
         self.lstm_layer = CustomLSTMLayer(128)
+        # Add other layers of your model here
+        self.dense_layer = tf.keras.layers.Dense(128, activation='relu')
+        self.rnn_layer = tf.keras.layers.SimpleRNN(128, activation='relu')
+        self.lstm_layer = tf.keras.layers.LSTM(128, activation='relu')
+
 
     def call(self, inputs):
         x = self.embedding_layer(inputs)
