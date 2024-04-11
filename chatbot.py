1ef3247e73ddb0186b7ac43c65b9fea83661a1b7
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2024-04-11 12:18:37 +0200

    resolve problem

--- chatbot.py
+++ chatbot.py
@@ -4,81 +4,17 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
-import pandas as pd
+import pickle
 
 cwd = os.getcwd()
 
+keras_model = tf.keras.models.load_model(cwd + '/chatbot_model.keras')
 
-# class CustomEmbeddingLayer(Layer):
-#     def __init__(self, input_dim, output_dim, input_length=None, **kwargs):
-#         super(CustomEmbeddingLayer, self).__init__()
-#         self.embedding = tf.keras.layers.Embedding(input_dim, output_dim, input_length=input_length, **kwargs)
-
-#     def call(self, inputs):
-#         return self.embedding(inputs)
-    
-# class CustomRNNLayer(Layer):
-#     def __init__(self, units, activation=None, **kwargs):
-#         super(CustomRNNLayer, self).__init__()
-#         self.rnn = tf.keras.layers.SimpleRNN(units, activation=activation, **kwargs)
-
-#     def call(self, inputs):
-#         return self.rnn(inputs)
-    
-# class CustomLSTMLayer(Layer):
-#     def __init__(self, units, activation=None, **kwargs):
-#         super(CustomLSTMLayer, self).__init__()
-#         self.lstm = tf.keras.layers.LSTM(units, activation=activation, **kwargs)
-
-#     def call(self, inputs):
-#         return self.lstm(inputs)
-
-
-# class CustomModel(tf.keras.Model):
-#     def __init__(self):
-#         super(CustomModel, self).__init__()
-#         self.embedding_layer = CustomEmbeddingLayer(input_dim, output_dim, input_length=max_seq_length)
-#         self.dense_layer = tf.keras.layers.Dense(128, activation='relu')
-#         self.rnn_layer = tf.keras.layers.SimpleRNN(128, activation='relu')
-#         # Add other layers of your model here
-#         self.lstm_layer = tf.keras.layers.LSTM(128, activation='relu')
-
-
-#     def call(self, inputs):
-#         x = self.embedding_layer(inputs)
-#         # Add your model logic here
-#         x = self.dense_layer(x)
-#         return x
-    
-#     def build(self, input_shape):
-#         super(CustomModel, self).build(input_shape)
-#         self.dense_layer.build(input_shape=(None, output_dim))
-
-
-# df = pd.read_csv(cwd + '/dialogs.txt', sep='\t')
-
-# # Charger le tokenizer
-# tokenizer = tf.keras.preprocessing.text.Tokenizer()
-# tokenizer.fit_on_texts(df)
-
-# # Définir la longueur maximale de séquence
-# max_seq_length = 50
-
-# # Définir les dimensions de l'embedding
-# input_dim = len(tokenizer.word_index) + 1
-# output_dim = 100
-
-# # Créer une instance du modèle personnalisé
-# model = CustomModel()
-# model.build((None, max_seq_length))
-
-# # Charger les poids du modèle pré-entraîné
-# model.load_weights(cwd + '/chatbot_model.h5')
-
-keras_model = tf.keras.models.load_model(cwd + '/chatbot_model.h5')
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 # Fonction pour prédire la réponse
-def predict_answer(input_text):
+def predict_answer(input_text, tokenizer, model, max_seq_length=20):
     input_sequence = tokenizer.texts_to_sequences([input_text])
     input_sequence_padded = pad_sequences(input_sequence, maxlen=max_seq_length, padding='post')
     predicted_index = np.argmax(model.predict(input_sequence_padded), axis=-1)[0]
@@ -91,5 +27,5 @@ user_input = st.text_input('Vous:')
 
 if st.button('Envoyer'):
     if user_input:
-        bot_response = predict_answer(user_input)
+        bot_response = predict_answer(user_input, tokenizer, keras_model)
         st.text_area('Chatbot:', value=bot_response, height=100)
