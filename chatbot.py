286b34e048a8173308dbdf130b62de2f94c0a7bd
grégory LOMBARD--PARADIS <gregory332000@hotmail.fr>
2024-04-11 10:26:38 +0200

    test

--- chatbot.py
+++ chatbot.py
@@ -51,7 +51,7 @@ output_dim = 100
 
 # Créer une instance du modèle personnalisé
 model = CustomModel()
-model.build((None, max_seq_length))
+model.build((None, output_dim))
 
 # Charger les poids du modèle pré-entraîné
 model.load_weights(cwd + '/chatbot_model.h5')
